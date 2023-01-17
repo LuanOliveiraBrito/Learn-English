80bba1637eb897c0821633d82e57b33c6c288b1a
Luan Oliveira de Brito Nunes <luanoliveirabritonunes@gmail.com>
2023-01-17 11:48:45 -0300

    First commit

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,9 @@
+import { Frase } from "../shared/frase.model";
+
+export const frases : Frase[] = [
+    { fraseEng: "I like to learn", frasePtBr: "eu gosto de aprender" },
+    { fraseEng: "I watch tv", frasePtBr: "eu assisto tv" },
+    { fraseEng: "How are you?", frasePtBr: "como está você?" },
+    { fraseEng: "I want be free", frasePtBr: "eu quero ser livre" },
+    { fraseEng: "I eat bread", frasePtBr: "eu como pão" },
+]
